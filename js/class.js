cfde180379ac2b915a3ef0d9f3e58aeb7b271e55
Frank Bennett <biercenator@gmail.com>
2013-12-09 14:01:56 +0900

    Finished display code for member/non-member lists

--- js/class.js
+++ js/class.js
@@ -1,6 +1,4 @@
 function buildMemberLists() {
-    // XXX fixme: watch consistency between this and class/student edit transactions
-    // if rows is nil, call the server.
     var adminID = getParameterByName('admin');
     var classID = getParameterByName('classid');
     var xhr = new XMLHttpRequest();
@@ -12,21 +10,21 @@ function buildMemberLists() {
     // Clear lists and rewrite
     var memberContainer = document.getElementById("members");
     var nonmemberContainer = document.getElementById("non-members");
-    for (var i=0,ilen=memberContainer.childNodes.length;i<ilen;i+=1) {
-        memberContainer.removeChild(memberContainer.childNodes[0]);
-    }
-    for (var i=0,ilen=nonmemberContainer.childNodes.length;i<ilen;i+=1) {
-        nonmemberContainer.removeChild(nonmemberContainer.childNodes[0]);
-    }
-    for (var i=0,ilen=rowsets[1].length;i<ilen;i+=1) {
-        var entry = document.createElement('div');
-        var checkBox = document.createElement('input');
-        checkBox.setAttribute('type', 'checkbox');
-        checkBox.setAttribute('value', rowsets[1][i].id);
-        entry.appendChild(checkBox);
-        var entryText = document.createTextNode(rowsets[1][i].name);
-        entry.appendChild(entryText);
-        nonmemberContainer.appendChild(entry);
+    var listContainers = [memberContainer, nonmemberContainer];
+    for (var i=0,ilen=listContainers.length;i<ilen;i+=1) {
+        for (var j=0,jlen=listContainers[i].childNodes.length;j<jlen;j+=1) {
+            listContainers[i].removeChild(listContainers[i].childNodes[0]);
+        }
+        for (var j=0,jlen=rowsets[i].length;j<jlen;j+=1) {
+            var entry = document.createElement('div');
+            var checkBox = document.createElement('input');
+            checkBox.setAttribute('type', 'checkbox');
+            checkBox.setAttribute('value', rowsets[i][j].id);
+            entry.appendChild(checkBox);
+            var entryText = document.createTextNode(rowsets[i][j].name);
+            entry.appendChild(entryText);
+            listContainers[i].appendChild(entry);
+        }
     }
 }
 
@@ -34,7 +32,16 @@ function addMembers () {
     var nonMembers = document.getElementById('non-members');
     for (var i=0,ilen=nonMembers.childNodes.length;i<ilen;i+=1) {
         if (nonMembers.childNodes[i].childNodes[0].checked) {
-            alert('Checked! '+nonMembers.childNodes[i].childNodes[0].value);
+            alert('To add! '+nonMembers.childNodes[i].childNodes[0].value);
+        }
+    }
+}
+
+function removeMembers () {
+    var members = document.getElementById('non-members');
+    for (var i=0,ilen=members.childNodes.length;i<ilen;i+=1) {
+        if (members.childNodes[i].childNodes[0].checked) {
+            alert('To remove! '+members.childNodes[i].childNodes[0].value);
         }
     }
 }
