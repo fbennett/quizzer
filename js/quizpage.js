12bf0afbf7c1ebaf2283b20faa72d139b6851c04
Frank Bennett <biercenator@gmail.com>
2013-12-15 10:02:56 +0900

    Move CSS for quiz page to file, more cleanup

--- js/quizpage.js
+++ js/quizpage.js
@@ -1,11 +1,8 @@
 var nextnodecounter = 0;
-var xmldocstr;
 var correctanscounter = 0;
-var randmarrcnt = 0;  
-var quesindex = 0;  
+var setvalue; 
 var getvalue;  
 var ansarry = new Array();
-var complete = false;
 var i;
 
 var classID = getParameterByName('classid');
@@ -17,60 +14,38 @@ if (!hostname) {
     hostname = 'our.law.nagoya-u.ac.jp';
 }
 
+// Get quiz data
 var xhr = new XMLHttpRequest();
 xhr.open('POST', 'http://' + hostname + ':3498/?cmd=quizdata&classid=' + classID+ '&id=' + studentID + '&key=' + studentKey + '&quizno=' + quizNumber, false);
 xhr.setRequestHeader("Content-type","text/plain");
 xhr.send(null);
 var quizData = JSON.parse(xhr.responseText);
 
-// Need something to extract the query string
-
-function UITest1() {
-  for (var i=0,ilen=quizData.questions.length;i<ilen;i+=1) {
-    remap = randomize(quizData.questions[i].questions);
-    quizData.questions[i].remap = remap;
-  }
-  var remap = randomize(quizData.questions);
-  quizData.remap = remap;
-  displaychild(quizData);
+function runQuiz() {
+    for (var i=0,ilen=quizData.questions.length;i<ilen;i+=1) {
+        remap = randomize(quizData.questions[i].questions);
+        quizData.questions[i].remap = remap;
+    }
+    var remap = randomize(quizData.questions);
+    quizData.remap = remap;
+    displaychild(quizData);
 }
-function OnSuccessCall(response) {  
-}  
-function OnErrorCall(response) {  
-}  
-function loadXMLString(str) {  
-	if (window.DOMParser) {  
-		parser = new DOMParser();  
-		xmlDoc =
-            parser.parseFromString(str, "text/xml");  
-    }  
-    else // Internet Explorer  
-	{  
-	    xmlDoc = new ActiveXObject("Microsoft.XMLDOM");  
-		xmlDoc.async = "false";  
-		xmlDoc.loadXML(txt);  
-	}  
-	return xmlDoc;  
-}  
-var newxmldoc;
+
 function displaychild(quizData) {  
-    // randomizeques(quizData);
     document.getElementById("nextButton").disabled = true;  
 	if (document.getElementById("nextButton").innerHTML == "Show&nbsp;Result") {  
         document.getElementById("nextButton").onclick = ShowResult;
         document.getElementById("nextButton").disabled = false;
     }
     if (quizData.questions.length == nextnodecounter) {
-      return;
+        return;
     }
     var question = quizData.questions[nextnodecounter];
     document.getElementById("question").innerHTML = marked.parse(question.rubric);
     
     // display question text  
 	document.getElementById("options").innerHTML = "";
-
     ansarry.push(question.correct);
-
     for (var i=0,ilen=question.questions.length;i<ilen;i+=1) {
         var choice = marked(question.questions[i]);
         var radioBtn = $('<li><div class="choice"><input name="r1" type="radio" value="'
@@ -82,6 +57,7 @@ function displaychild(quizData) {
         checkradiobtn();
     }
 }  
+
 function FunNextNode() {
 	nextnodecounter = nextnodecounter + 1;
     var pos = (nextnodecounter-1);
@@ -95,18 +71,18 @@ function FunNextNode() {
     }
     displaychild(quizData);  
 }  
-function checkradiobtn()  
-{  
+
+function checkradiobtn () {  
 	var radios =
         document.getElementsByTagName('input');  
-    var value;  
-    for (var i = 0; i < radios.length; i++) {  
-	    if (radios[i].type == 'radio' && radios[i].checked) {  
+    var value;
+    for (var i = 0; i < radios.length; i++) {
+	    if (radios[i].type == 'radio' && radios[i].checked) {
 		    document.getElementById("nextButton").disabled = true;  
         }
-    }  
-} 
-var setvalue; 
+    }
+}
+
 function enablebtn(setvalue) {  
 	document.getElementById("nextButton").disabled = false;  
     getvalue = setvalue;  
@@ -124,42 +100,35 @@ function ShowResult() {
     // XXX Secondary API reads the results and compiles to CSV
     // XXX Done.
 }
-// function to get random index of questions  
-var tempcount = 0;  
-var tempxmldoc;  
-var randquearr = new Array();  
 
 function randomize(array) {
-  var orig_order = [];
-  for (var i=0,ilen=array.length;i<ilen;i+=1) {
-    orig_order.push(i);
-  }
-  var new_order = orig_order.slice();
-  var currentIndex = array.length
-      , temporaryValue
-      , randomIndex
-      , temporaryPos
-    ;
+    var orig_order = [];
+    var new_order = orig_order.slice();
+    var currentIndex = array.length;
+    var temporaryValue;
+    var randomIndex;
+    var temporaryPos;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+    for (var i=0,ilen=array.length;i<ilen;i+=1) {
+        orig_order.push(i);
+    }
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    temporaryPos = new_order[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    new_order[currentIndex] = new_order[randomIndex];
-    array[randomIndex] = temporaryValue;
-    new_order[randomIndex] = temporaryPos;
-  }
-  var remap = {};
-  for (var i=0,ilen=array.length;i<ilen;i+=1) {
-    remap[new_order[i]] = orig_order[i];
-  }
-  return remap;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        temporaryPos = new_order[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        new_order[currentIndex] = new_order[randomIndex];
+        array[randomIndex] = temporaryValue;
+        new_order[randomIndex] = temporaryPos;
+    }
+    var remap = {};
+    for (var i=0,ilen=array.length;i<ilen;i+=1) {
+        remap[new_order[i]] = orig_order[i];
+    }
+    return remap;
 }
-
