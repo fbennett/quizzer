84d278220981932963039969b6e5f04f81a7c8e1
Frank Bennett <biercenator@gmail.com>
2013-12-27 17:21:04 +0900

    Mail message seems about right now.

--- api/admin/quiz/sendquiz/matchExec.js
+++ api/admin/quiz/sendquiz/matchExec.js
@@ -17,54 +17,68 @@
             port = '';
             stub = pathName.replace(/(.*\/).*/, '$1/quiz.html');
         } 
-        var front = "We have prepared a quiz to help you check and improve your English writing ability.\n\n"
+        var front_template = "@@NAME@@,\n\nWe have prepared a fresh quiz to help you check and improve your English writing ability.\n\n"
             + "Click on the link below to take the quiz:\n\n"
         var template_link = "    http://" + hostname + port + stub
             + "?studentid=@@STUDENT_ID@@&studentkey=@@STUDENT_KEY@@"
             + "&classid=" + classID 
             + "&quizno=@@QUIZ_NUMBER@@\n\n"
         var template_middle = "The links to your past quizzes for this class have changed. Here are the new links:\n\n"
-            + "@@LINKS@@\n\n"
+            + "@@LINKS@@"
         var back = "Sincerely yours,\n"
             + "The Academic Writing team"
-        sys.db.all('SELECT m.studentID,m.studentKey,s.name FROM memberships AS m JOIN students AS s ON s.studentID=m.studentID JOIN quizzes AS q ON q.classID=m.classID WHERE m.classID=? AND q.quizNumber=? AND m.studentID NOT IN (SELECT studentID FROM answers AS a WHERE a.classID=? AND a.quizNumber=?);',[classID,quizNumber,classID,quizNumber],function(err,rows){
+        // Okay. We need to refresh the student keys before running the code below.
+        sys.db.all('SELECT m.studentID,s.name FROM memberships AS m JOIN students AS s ON s.studentID=m.studentID JOIN quizzes AS q ON q.classID=m.classID WHERE m.classID=? AND q.quizNumber=? AND m.studentID NOT IN (SELECT studentID FROM answers AS a WHERE a.classID=? AND a.quizNumber=?);',[classID,quizNumber,classID,quizNumber],function(err,rows){
             if (err||!rows) {return oops(response,err,'quiz/sendquiz(1)')};
+            var datalst = [];
             for (var i=0,ilen=rows.length;i<ilen;i+=1) {
                 var row = rows[i];
-                var studentID = row.studentID;
-                var studentKey = row.studentKey;
-                var link = template_link
-                    .replace(/@@STUDENT_ID@@/g,studentID)
-                    .replace(/@@STUDENT_KEY@@/g,studentKey)
-                    .replace(/@@QUIZ_NUMBER@@/g,quizNumber);
-                console.log("Send to: "+row.name);
-                var pastLinks = 0;
-                sys.db.all('SELECT quizNumber FROM questions WHERE classID=? AND quizNumber IN (SELECT quizNumber FROM answers WHERE classID=? AND studentID=?) GROUP BY quizNumber',[classID,classID,studentID],function(err,rows){
+                var studentKey = sys.getRandomKey(8,36);
+                // All wrong.
+                // This is a list of students to receive mail in this class.
+                // Keys need to be updated for all keys of these students, in this class.
+                sys.db.run('UPDATE memberships SET studentKey=? WHERE classID=? AND studentID=?',[studentKey,classID,row.studentID],function(err){
                     if (err) {return oops(response,err,'quiz/sendquiz(2)')};
-                    var middle = '';
-                    var msg;
-                    if (!rows || !rows.length) {
-                        // plain message
-                        msg = front + link + back;
-                    } else {
-                        // add note of past quiz links
-                        var links = '';
-                        for (var i=0,ilen=rows.length;i<ilen;i+=1) {
-                            var row = rows[i];
-                            quizNumber = row.quizNumber;
-                            links += template_link
-                                .replace(/@@STUDENT_ID@@/g,studentID)
-                                .replace(/@@STUDENT_KEY@@/g,studentKey)
-                                .replace(/@@QUIZ_NUMBER@@/g,quizNumber);
-                        }
-                        var middle = template_middle
-                            .replace(/@@LINKS@@/g,links);
-                        msg = front + link + middle + back;
-                    }
-                    console.log(msg);
+                    // Good, so this does that. Send the mail message.
+                    sendMail(quizNumber,row.studentID,studentKey,row.name);
                 });
+                
             }
-        })
+        });
+        
+        function sendMail (quizNumber,studentID,studentKey,name) {
+            var link = template_link
+                .replace(/@@STUDENT_ID@@/g,studentID)
+                .replace(/@@STUDENT_KEY@@/g,studentKey)
+                .replace(/@@QUIZ_NUMBER@@/g,quizNumber);
+            console.log("Send to: "+name);
+            var front = front_template.replace(/@@NAME@@/,name)
+            var pastLinks = 0;
+            sys.db.all('SELECT q.quizNumber FROM questions AS q JOIN quizzes AS qz ON qz.classID=q.classID AND qz.quizNumber=q.quizNumber WHERE q.classID=? AND NOT q.quizNumber=? AND qz.sent GROUP BY q.quizNumber',[classID,quizNumber],function(err,rows){
+                if (err) {return oops(response,err,'quiz/sendquiz(3)')};
+                var middle = '';
+                var msg;
+                if (!rows || !rows.length) {
+                    // plain message
+                    msg = front + link + back;
+                } else {
+                    // add note of past quiz links
+                    var links = '';
+                    for (var i=0,ilen=rows.length;i<ilen;i+=1) {
+                        var row = rows[i];
+                        quizNumber = row.quizNumber;
+                        links += template_link
+                            .replace(/@@STUDENT_ID@@/g,studentID)
+                            .replace(/@@STUDENT_KEY@@/g,studentKey)
+                            .replace(/@@QUIZ_NUMBER@@/g,quizNumber);
+                    }
+                    var middle = template_middle
+                        .replace(/@@LINKS@@/g,links);
+                    msg = front + link + middle + back;
+                }
+                console.log(msg);
+            });
+        }
         //
         response.writeHead(500, {'Content-Type': 'text/plain'});
         response.end("Testing ...");
