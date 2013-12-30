e531b5f2a56eb859bf9f4d0e4d7ca1ef4e79ee30
Frank Bennett <biercenator@gmail.com>
2013-12-30 12:40:33 +0900

    Some further fixes to pending counts in admin class page

--- api/admin/class/readquizzes/matchExec.js
+++ api/admin/class/readquizzes/matchExec.js
@@ -2,36 +2,31 @@
     var cogClass = function () {};
     cogClass.prototype.exec = function (params, request, response) {
         var oops = this.utils.apiError;
+        var classID = params.classid;
         var db = this.sys.db;
-        var sql = "SELECT q.quizNumber,q.sent,COUNT(s.studentID) AS enroled,COUNT(a.choice) AS submitted "
-            + "FROM quizzes AS q "
-            + "JOIN classes AS c ON q.classID=c.classID "
-            + "JOIN memberships AS m ON c.classID=m.classID "
-            + "JOIN students AS s ON m.studentID=s.studentID "
-            + "LEFT JOIN answers AS a ON a.studentID=s.studentID "
-            + "AND a.classID=q.classID "
-            + "AND a.quizNumber=q.quizNumber "
-            + "WHERE q.classID=? "
-            + "GROUP BY q.quizID "
-            + "ORDER BY q.quizNumber;"
-        console.log("XX>> "+sql);
-        db.all(sql,[params.classid],function(err,rows){
+        var sql = 'SELECT qz.quizNumber,CASE WHEN qz.sent IS NULL OR qz.sent=0 THEN -1 ELSE COUNT(res.pending) END AS pending '
+            + 'FROM quizzes AS qz LEFT JOIN ('
+            +     'SELECT q.quizNumber AS pending FROM memberships AS m '
+            +     'JOIN questions AS q ON q.classID=m.classID '
+            +     'LEFT JOIN answers AS a ON a.classID=q.classID AND a.quizNumber=q.quizNumber AND a.questionNumber=q.questionNumber AND a.studentID=m.studentID '
+            +     'WHERE q.classID=? AND a.quizNumber IS NULL '
+            +     'GROUP BY q.classID,q.quizNumber,m.studentID'
+            + ') AS res ON res.pending=qz.quizNumber '
+            + 'WHERE qz.classID=? '
+            + "GROUP BY qz.classID,qz.quizNumber "
+            + "ORDER BY qz.quizNumber;"
+        console.log("XX> "+sql);
+        db.all(sql,[classID,classID],function(err,rows){
             if (err||!rows) {return oops(response,err,'class/readquizzes')};
             var retRows = [];
             var hasNew = false;
             var maxval = 0;
             for (var i=0,ilen=rows.length;i<ilen;i+=1) {
                 var row = rows[i];
-                if (row.sent) {
-                    if (row.enroled > row.submitted) {
-                        retRows.push({number:row.quizNumber,isnew:(row.enroled - row.submitted)});
-                    } else {
-                        retRows.push({number:row.quizNumber,isnew:0});
-                    }
-                } else {
+                if (row.pending == -1) {
                     hasNew = true;
-                    retRows.push({number:row.quizNumber,isnew:-1});
                 }
+                retRows.push({number:row.quizNumber,isnew:row.pending});
                 maxval = row.quizNumber;
             }
             if (rows.length && !hasNew) {
