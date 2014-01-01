96a6ea2f3bdfefc38da3af42addc1a585afa788b
Frank Bennett <biercenator@gmail.com>
2014-01-01 09:52:55 +0900

    Consternation smilie

--- js/local.js
+++ js/local.js
@@ -45,6 +45,7 @@ function apiRequest (url, obj, returnAsString) {
 function markdown (txt) {
     if (!txt) return '<p>&nbsp;</p>';
     txt = txt.replace(/(:-?\))/g,'(\u0298\u203f\u0298)');
+    txt = txt.replace(/(:-/)/g,'_(\u0361\u0e4f\u032f\u0361\u0e4f)_');
     txt = txt.replace(/\(\(([a-zA-Z1-9])\s+(.*?)\)\)/g, '<span class="quizzer-highlight">(($1)) $2</span>');
     txt = txt.replace(/\(\(([a-zA-Z1-9])\)\)/g, function (aChar) {
         var c, val, offset;
