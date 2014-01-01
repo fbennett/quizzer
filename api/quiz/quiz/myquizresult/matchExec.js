e1089814825091d961a0d5141a6b2126d1b458fd
Frank Bennett <biercenator@gmail.com>
2014-01-01 10:41:32 +0900

    Set tracking variable in outer scope

--- api/quiz/quiz/myquizresult/matchExec.js
+++ api/quiz/quiz/myquizresult/matchExec.js
@@ -6,12 +6,13 @@
         var classID = params.classid;
         var quizNumber = params.quizno;
         var studentID = params.studentid;
+        var mistakeCount;
         var sql = 'SELECT a.questionNumber,q.correct,a.choice FROM answers AS a JOIN questions AS q ON q.classID=a.classID AND q.quizNumber=a.quizNumber AND q.questionNumber=a.questionNumber WHERE a.classID=? AND a.quizNumber=? AND a.studentID=? AND NOT a.choice=q.correct';
         sys.db.all(sql,[classID,quizNumber,studentID],function(err,rows){
             if (err||!rows) {return oops(response,err,'*quiz/myquizresult(1)')};
             if (rows.length) {
                 var items = [];
-                var mistakeCount = rows.length;
+                mistakeCount = rows.length;
                 for (var i=0,ilen=rows.length;i<ilen;i+=1) {
                     var row = rows[i];
                     var fields = ['qOneID','qTwoID','qThreeID','qFourID'];
