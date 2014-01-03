18de2224eb6e1961ac06bbbd0f7fe3e92fa633a5
Frank Bennett <biercenator@gmail.com>
2014-01-03 18:58:55 +0900

    Infrastructure for database upgrade steps, with two upgrades
    The SQL upgrade code introduced here was omitted from the previous
    checkin. This checkin introduces a second upgrade step, to add
    an optional examName field to quizzes table.

--- resource/schema.sql
+++ resource/schema.sql
@@ -1,13 +1,13 @@
--- 2
+-- 3
 
-CREATE TABLE admin (
+CREATE TABLE IF NOT EXISTS admin (
        adminID INTEGER PRIMARY KEY,
        name TEXT,
        adminKey TEXT,
        role INTEGER,
        interval INTEGER);
-CREATE UNIQUE INDEX admin_key_idx ON admin(adminKey);
-CREATE UNIQUE INDEX admin_name_idx ON admin(name);
+CREATE UNIQUE INDEX IF NOT EXISTS admin_key_idx ON admin(adminKey);
+CREATE UNIQUE INDEX IF NOT EXISTS admin_name_idx ON admin(name);
 
 CREATE TABLE IF NOT EXISTS students (studentID INTEGER PRIMARY KEY,name TEXT,email TEXT,privacy INTEGER DEFAULT 0, lang TEXT DEFAULT 'en');
 
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS classes (classID INTEGER PRIMARY KEY,name TEXT);
 
 CREATE TABLE IF NOT EXISTS memberships (membershipID INTEGER PRIMARY KEY,classID INTEGER,studentID INTEGER, studentKey TEXT);
 
-CREATE TABLE IF NOT EXISTS quizzes (quizID INTEGER PRIMARY KEY, classID INTEGER, quizNumber INTEGER, sent BOOLEAN);
+CREATE TABLE IF NOT EXISTS quizzes (quizID INTEGER PRIMARY KEY, classID INTEGER, quizNumber INTEGER, sent BOOLEAN, examName TEXT);
 CREATE UNIQUE INDEX IF NOT EXISTS quizzes_idx ON quizzes(classID,quizNumber);
 
 CREATE TABLE IF NOT EXISTS questions (questionID INTEGER PRIMARY KEY, classID INTEGER, quizNumber INTEGER, questionNumber INTEGER, correct INTEGER, rubricID INTEGER, qOneID INTEGER, qTwoID INTEGER, qThreeID INTEGER, qFourID INTEGER);
