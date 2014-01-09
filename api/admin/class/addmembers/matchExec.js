2dde338d909bfd69edcc3a097957dd61956ff5b8
Frank Bennett <biercenator@gmail.com>
2014-01-09 17:18:57 +0900

    Fix broken table insert

--- api/admin/class/addmembers/matchExec.js
+++ api/admin/class/addmembers/matchExec.js
@@ -7,7 +7,7 @@
         var classID = params.classid;
         var sql = [];
         var getClassMemberships = this.utils.getClassMemberships;
-        var stmt = db.prepare('INSERT INTO memberships VALUES(NULL,?,?,?);');
+        var stmt = db.prepare('INSERT INTO memberships VALUES(NULL,?,?,?,NULL);');
         for (var i=0,ilen=params.addmembers.length;i<ilen;i+=1) {
             var addmemberID = params.addmembers[i];
             var addmemberKey = getRandomKey(8,36);
