bc99fc9864c8d59428b983068ad7b87454ff5759
Frank Bennett <biercenator@gmail.com>
2014-01-10 13:13:21 +0900

    Show edited students in non-members list for all classes.

--- api/admin/students/addstudent/matchExec.js
+++ api/admin/students/addstudent/matchExec.js
@@ -2,26 +2,35 @@
     var cogClass = function () {};
     cogClass.prototype.exec = function (params, request, response) {
         var oops = this.utils.apiError;
+        var sys = this.sys;
         var studentID = params.studentid;
         var name = params.name;
         var email = params.email;
         var lang = params.lang;
         var privacy = params.privacy ? 1 : 0;
+        var adminID = sys.admin[params.admin].id;
         console.log("Got param: "+params.privacy);
         if (studentID) {
             var db = this.sys.db;
             db.run('INSERT OR REPLACE INTO students VALUES (?,?,?,?,?)',[studentID,name,email,privacy,lang],function(err){
-                if (err) {return oops(response,err,'students/addstudent')};
-                sendStudents();
+                if (err) {return oops(response,err,'students/addstudent(1)')};
+                exposeStudent(studentID);
             })
         } else {
             var db = this.sys.db;
             db.run('INSERT INTO students VALUES (NULL,?,?,?,?)',[name,email,privacy,lang],function(err){
-                if (err) {return oops(response,err,'students/addstudent')};
-                sendStudents();
+                if (err) {return oops(response,err,'students/addstudent(2)')};
+                exposeStudent(this.lastID);
             });
         }
-        // XXX This is a duplicate of students/readstudents
+
+        function exposeStudent (studentID) {
+            db.get('INSERT OR IGNORE INTO showing (showID,adminID,classID,studentID) SELECT NULL AS showID,? AS adminID,classID,? AS studentID FROM classes',[adminID,studentID],function(err,rows) {
+                if (err) {return oops(response,err,'students/addstudent(3)')};
+                sendStudents();                
+            });
+        }
+
         function sendStudents () {
             db.all('SELECT studentID,name,email,privacy,lang FROM students',function(err,rows){
                 if (err||!rows) {return oops(response,err,'students/addstudent')};
