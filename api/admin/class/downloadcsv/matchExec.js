89e3e236bb5e6a69834ecd86a6d0c3dc6fd3d112
Frank Bennett <biercenator@gmail.com>
2014-01-11 13:56:57 +0900

    Provide a meaningful filename on CSV download

--- api/admin/class/downloadcsv/matchExec.js
+++ api/admin/class/downloadcsv/matchExec.js
@@ -6,14 +6,17 @@
         var classID = params.classid;
         var sys = this.sys;
         var studentInfo = [];
-        console.log("ATTEMPTING DOWNLOAD FOR: "+classID);
-        sys.db.all('SELECT s.name,s.email FROM memberships AS m JOIN students AS s ON s.studentID=m.studentID WHERE m.classID=? AND s.privacy=0',[classID],function(err,rows){
+        sys.db.all('SELECT s.name,s.email,c.name AS className FROM memberships AS m JOIN students AS s ON s.studentID=m.studentID JOIN classes AS c ON c.classID=m.classID WHERE m.classID=? AND (s.privacy IS NULL OR s.privacy=0)',[classID],function(err,rows){
             if (err||!rows) {return oops(response,err,'class/downloadcsv(1)')};
-            for (var i=0,ilen=rows.length;i<ilen;i+=1) {
-                var row = rows[i];
-                studentInfo.push('"' + row.name + '","' + row.email + '"');
+            var className = "Empty file";
+            if (rows.length) {
+                var className = rows[0].className;
+                for (var i=0,ilen=rows.length;i<ilen;i+=1) {
+                    var row = rows[i];
+                    studentInfo.push('"' + row.name + '","' + row.email + '"');
+                }
             }
-            response.writeHead(200, {'Content-Type': 'text/csv'});
+            response.writeHead(200, {'Content-Type': 'text/csv','Content-Disposition':'attachment; filename="' + className + ' (roster).csv"'});
             response.end(studentInfo.join('\n'));
         });
     }
