fdbaae2d2fccf635fb7803522dabab97b60e9607
Frank Bennett <biercenator@gmail.com>
2014-01-14 10:44:56 +0900

    Recast br tags in source text view

--- js/admin-quiz.js
+++ js/admin-quiz.js
@@ -643,7 +643,7 @@ function displayQuestion (qobj, questionNumber) {
     rubric.setAttribute('class','rubric-wrapper');
     rubric.innerHTML = '<div class="raw-text-wrapper">'
         + '<div class="raw-text-marker">Source</div>'
-        + '<pre class="raw-text">' + qobj.rubric+ '</pre>'
+        + '<pre class="raw-text">' + qobj.rubric.replace(/<br\/?>/g,'&lt;br/&gt') + '</pre>'
         + '</div>'
         + '<div class="rubric">' + markdown(qobj.rubric) + '</div>'
     node.appendChild(rubric);
