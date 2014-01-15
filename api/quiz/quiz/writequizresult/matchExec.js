001c6055023cc9223657b6e51301549df3bff350
Frank Bennett <biercenator@gmail.com>
2014-01-15 22:59:52 +0900

    Tighten up database schema a little, and make question and answer writes a little more robust

--- api/quiz/quiz/writequizresult/matchExec.js
+++ api/quiz/quiz/writequizresult/matchExec.js
@@ -22,7 +22,21 @@
         var resultUrl = 'http://' + hostname + port + stub + '?classid=' + classID+ '&studentid=' + studentID + '&studentkey=' + studentKey + '&quizno=' + quizNumber;
         for (var questionNumber in quizResult) {
             var choice = quizResult[questionNumber];
-            sys.db.run('INSERT OR REPLACE INTO answers (questionID,studentID,choice) SELECT q.questionID,?,? FROM questions AS q WHERE classID=? AND quizNumber=? AND questionNumber=?',[studentID,choice,classID,quizNumber,questionNumber],function(err){
+            checkAnswer(classID,quizNumber,questionNumber,studentID);
+        }
+        function checkAnswer(classID,quizNumber,questionNumber,studentID) {
+            sys.db.get('SELECT a.answerID FROM answers AS a JOIN questions AS q ON q.questionID=a.questionID WHERE q.classID=? AND q.quizNumber=? AND q.questionNumber=? AND a.studentID=?',[classID,quizNumber,questionNumber,studentID],function(err,row){
+                if (err) {return oops(response,err,'*quiz/writequizresult(1)')}
+                if (row && row.answerID) {
+                    saveAnswer(row.answerID,classID,quizNumber,questionNumber,studentID,choice);
+                } else {
+                    saveAnswer(null,classID,quizNumber,questionNumber,studentID,choice);
+                }
+            });
+
+        }
+        function saveAnswer (answerID,classID,quizNumber,questionNumber,studentID,choice) {
+            sys.db.run('INSERT OR REPLACE INTO answers (answerID,questionID,studentID,choice) SELECT ?,q.questionID,?,? FROM questions AS q WHERE classID=? AND quizNumber=? AND questionNumber=?',[answerID,studentID,choice,classID,quizNumber,questionNumber],function(err){
                 if (err) {return oops(response,err,'*quiz/writequizresult')};
                 response.writeHead(200, {'Content-Type': 'text/plain'});
                 response.end(resultUrl);
--- resource/schema.sql
+++ resource/schema.sql
@@ -1,4 +1,4 @@
--- 8
+-- 9
 
 CREATE TABLE IF NOT EXISTS admin (
        adminID INTEGER PRIMARY KEY,
@@ -19,11 +19,28 @@ CREATE TABLE IF NOT EXISTS memberships (membershipID INTEGER PRIMARY KEY,classID
 CREATE TABLE IF NOT EXISTS quizzes (quizID INTEGER PRIMARY KEY, classID INTEGER, quizNumber INTEGER, sent BOOLEAN, examName TEXT, examDate TEXT);
 CREATE UNIQUE INDEX IF NOT EXISTS quizzes_idx ON quizzes(classID,quizNumber);
 
-CREATE TABLE IF NOT EXISTS questions (questionID INTEGER PRIMARY KEY, classID INTEGER, quizNumber INTEGER, questionNumber INTEGER, correct INTEGER, rubricID INTEGER, qOneID INTEGER, qTwoID INTEGER, qThreeID INTEGER, qFourID INTEGER);
+CREATE TABLE IF NOT EXISTS questions (
+       questionID INTEGER PRIMARY KEY,
+       classID INTEGER,
+       quizNumber INTEGER,
+       questionNumber INTEGER,
+       correct INTEGER,
+       rubricID INTEGER,
+       qOneID INTEGER,
+       qTwoID INTEGER,
+       qThreeID INTEGER,
+       qFourID INTEGER
+);
 CREATE UNIQUE INDEX IF NOT EXISTS questions_idx ON questions(classID,quizNumber,questionNumber);
 
-CREATE TABLE IF NOT EXISTS answers (answerID INTEGER PRIMARY KEY, questionID INTEGER, studentID INTEGER, choice INTEGER);
-CREATE UNIQUE INDEX IF NOT EXISTS answers_idx ON answers(questionID,studentID,choice);
+CREATE TABLE IF NOT EXISTS answers (
+       answerID INTEGER PRIMARY KEY,
+       questionID INTEGER,
+       studentID INTEGER,       
+       choice INTEGER,
+       UNIQUE(questionID,studentID,choice),
+       FOREIGN KEY (questionID) REFERENCES questions(questionID)
+);
 
 CREATE TABLE IF NOT EXISTS comments(
   commentID INT PRIMARY KEY,
