4d033642bc7e0a52a678043a088dd8d8be659a75
Frank Bennett <biercenator@gmail.com>
2014-01-26 22:17:35 +0900

    Untested code for displaying rules in student returns

--- api/quiz/quiz/myquizresult/matchExec.js
+++ api/quiz/quiz/myquizresult/matchExec.js
@@ -6,34 +6,41 @@
         var classID = params.classid;
         var quizNumber = params.quizno;
         var studentID = params.studentid;
-        var mistakeCount;
-        var sql = 'SELECT questionNumber,correct,choice '
-            + 'FROM answers '
-            + 'NATURAL JOIN questions '
-            + 'JOIN quizzes USING (quizID) '
-            + 'WHERE quizzes.classID=? AND quizzes.quizNumber=? AND answers.studentID=? AND NOT answers.choice=questions.correct';
-        sys.db.all(sql,[classID,quizNumber,studentID],function(err,rows){
-            if (err||!rows) {return oops(response,err,'*quiz/myquizresult(1)')};
-            if (rows.length) {
-                var items = [];
-                mistakeCount = rows.length;
-                for (var i=0,ilen=rows.length;i<ilen;i+=1) {
-                    var row = rows[i];
-                    var fields = ['qOneID','qTwoID','qThreeID','qFourID'];
-                    var questionNumber = row.questionNumber;
-                    var wrongAnswerFieldName = fields[row.choice];
-                    var rightAnswerFieldName = fields[row.correct];
-                    getMistakes(classID,quizNumber,questionNumber,studentID,items);
+        var studentLang;
+        var items = [];
 
+        function getStudentLanguage () {
+            var sql = 'SELECT lang FROM students WHERE studentID=?';
+            sys.db.get(sql,[studentID],function(err,row){
+                if (err) {return oops(response,err,'*quiz/myquizresult(0)')};
+                studentLang = row.lang;
+                setEntryPlaceholders();
+            });
+        }
+
+        function setEntryPlaceholders () {
+            var sql = 'SELECT questionNumber '
+                + 'FROM answers '
+                + 'NATURAL JOIN questions '
+                + 'JOIN quizzes USING (quizID) '
+                + 'WHERE quizzes.classID=? AND quizzes.quizNumber=? AND answers.studentID=? AND NOT answers.choice=questions.correct';
+            sys.db.all(sql,[classID,quizNumber,studentID],function(err,rows){
+                if (err||!rows) {return oops(response,err,'*quiz/myquizresult(1)')};
+                if (rows.length) {
+                    for (var i=0,ilen=rows.length;i<ilen;i+=1) {
+                        var row = rows[i];
+                        items.push({questionNumber:row.questionNumber});
+                    }
+                    getMistakes(0,items.length);
+                } else {
+                    var data = JSON.stringify([])
+                    response.writeHead(200, {'Content-Type': 'application/json'});
+                    response.end(data);
                 }
-            } else {
-                var data = JSON.stringify([])
-                response.writeHead(200, {'Content-Type': 'application/json'});
-                response.end(data);
-            }
-        });
+            });
+        }
 
-        function getMistakes (classID,quizNumber,questionNumber,studentID,items) {
+        function getMistakes (pos,limit) {
             var sql = 'SELECT rubric.string AS rubric,right.string AS right,wrong.string AS wrong,wrong.choice AS wrongChoice '
                 + 'FROM quizzes '
                 + 'NATURAL JOIN questions '
@@ -54,37 +61,87 @@
                 +     'JOIN strings ON strings.stringID=choices.stringID'
                 + ') AS wrong USING (questionID) '
                 + 'WHERE quizzes.classID=? AND quizzes.quizNumber=? AND questions.questionNumber=? AND answers.studentID=?'
-            console.log("XXX "+sql)
-            console.log(classID+" "+quizNumber+" "+questionNumber+" "+studentID);
-             sys.db.get(sql,[classID,quizNumber,questionNumber,studentID],function(err,row) {
+            var questionNumber = items[pos].questionNumber;
+            sys.db.get(sql,[classID,quizNumber,questionNumber,studentID],function(err,row) {
                 if (err||!row) {return oops(response,err,'*quiz/myquizresult(2)')}
-                var rubric = row.rubric;
-                var right = row.right;
-                var wrong = row.wrong;
-                var wrongChoice = row.wrongChoice;
-                getGoodAnswerStudents(classID,quizNumber,questionNumber,wrongChoice,items,rubric,right,wrong);
+                items[pos].rubric = row.rubric;
+                items[pos].right = row.right;
+                items[pos].wrong = row.wrong;
+                items[pos].wrongChoice = row.wrongChoice;
+                pos += 1;
+                if (pos === items.length) {
+                    getGoodAnswerStudents(0,limit);
+                } else {
+                    getMistakes(pos,limit);
+                }
             });
-        }
+        };
 
-        function getGoodAnswerStudents(classID,quizNumber,questionNumber,wrongChoice,items,rubric,right,wrong) {
+        function getGoodAnswerStudents(pos,limit) {
             var sql = 'SELECT name '
                 + 'FROM answers '
                 + 'NATURAL JOIN questions '
                 + 'JOIN quizzes USING(quizID) '
                 + 'JOIN students USING(studentID) '
                 + 'WHERE quizzes.classID=? AND quizzes.quizNumber=? AND questions.questionNumber=? AND questions.correct=answers.choice'
+            var questionNumber = items[pos].questionNumber;
             sys.db.all(sql,[classID,quizNumber,questionNumber],function(err,rows){
                 if (err||!rows) {return oops(response,err,'*quiz/myquizresult(3)')}
-                var goodAnswerStudents = [];
+                items[pos].goodAnswerStudents = [];
                 for (var i=0,ilen=rows.length;i<ilen;i+=1) {
                     var row = rows[i];
-                    goodAnswerStudents.push(row.name);
+                    items[pos].goodAnswerStudents.push(row.name);
                 }
-                getComments(classID,quizNumber,questionNumber,wrongChoice,items,rubric,right,wrong,goodAnswerStudents);
+                pos += 1;
+                if (pos === items.length) {
+                    getRules(0,limit);
+                } else {
+                    getGoodAnswerStudents(pos,limit);
+                }
             });
-        }
+        };
 
-        function getComments(classID,quizNumber,questionNumber,wrongChoice,items,rubric,right,wrong,goodAnswerStudents) {
+
+        function getRules(pos,limit) {
+            var sql = 'SELECT r.string AS ruleText,'
+                + 'CASE WHEN rtO.string IS NOT NULL THEN rtO.string ELSE rtE.string END AS ruleGloss '
+                + 'FROM quizzes '
+                + 'NATURAL JOIN questions USING(quizID) '
+                + 'JOIN choices USING(questionID) '
+                + 'JOIN answers USING(questionID) '
+                + 'JOIN students USING(studentID) '
+                + 'JOIN rulesToChoices USING(choiceID) '
+                + 'JOIN rules USING(ruleID) '
+                + 'JOIN ruleStrings AS r USING(ruleStringID) '
+                + 'LEFT JOIN ('
+                +   'SELECT ruleID,string '
+                +   'FROM ruleTranslations '
+                +   'WHERE lang=\'en\''
+                + ') AS rtE USING(ruleID) '
+                + 'LEFT JOIN ('
+                +   'SELECT ruleID,string '
+                +   'FROM ruleTranslations '
+                +   'WHERE lang=?'
+                + ') AS rtO USING(ruleID) '
+                + 'WHERE quizzes.classID=? AND quizNumber=? AND questionNumber=?';
+            var questionNumber = items[pos].questionNumber;
+            sys.db.all(sql,[studentLang,classID,quizNumber,questionNumber],function(err,rows){
+                if (err||!rows) {return oops(response,err,'*quiz/myquizresult(4)')};
+                items[pos].rules = [];
+                for (var i=0,ilen=rows.length;i<ilen;i+=1) {
+                    var row = rows[i];
+                    items[pos].rules.push({ruleText:row.ruleText,ruleGloss:row.ruleGloss});
+                }
+                pos += 1;
+                if (pos === items.length) {
+                    getComments(0,limit);
+                } else {
+                    getRules(pos,limit);
+                }
+            });
+        };
+
+        function getComments(pos,limit) {
             var sql = 'SELECT admin.name AS commenter,s.string AS comment '
                 + 'FROM quizzes '
                 + 'NATURAL JOIN questions '
@@ -93,28 +150,24 @@
                 + 'JOIN admin USING(adminID) '
                 + 'LEFT JOIN strings AS s ON s.stringID=comments.stringID '
                 + 'WHERE quizzes.classID=? AND quizzes.quizNumber=? AND questions.questionNumber=? AND choices.choice=?'
+            var questionNumber = items[pos].questionNumber;
+            var wrongChoice = items[pos].wrongChoice;
             sys.db.all(sql,[classID,quizNumber,questionNumber,wrongChoice],function(err,rows){
-                if (err||!rows) {return oops(response,err,'*quiz/myquizresults(4)')};
-                var comments = [];
+                if (err||!rows) {return oops(response,err,'*quiz/myquizresults(5)')};
+                items[pos].comments = [];
                 for (var i=0,ilen=rows.length;i<ilen;i+=1) {
                     var row = rows[i];
-                    comments.push({commenter:row.commenter,comment:row.comment});
+                    items[pos].comments.push({commenter:row.commenter,comment:row.comment});
                 }
-                items.push({
-                    rubric:rubric,
-                    right:right,
-                    wrong:wrong,
-                    goodAnswerStudents:goodAnswerStudents,
-                    comments:comments
-                });
-                mistakeCount += -1;
-                if (!mistakeCount) {
-                    var data = JSON.stringify(items);
+                pos += 1;
+                if (pos === items.length) {
                     response.writeHead(200, {'Content-Type': 'application/json'});
-                    response.end(data);
+                    response.end(JSON.stringify(items));
+                } else {
+                    getComments(pos,limit);
                 }
             });
-        }
+        };
     }
     exports.cogClass = cogClass;
 })();
