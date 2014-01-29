1535a77e8c13eabb7aac3ef849900a91f440575c
Frank Bennett <biercenator@gmail.com>
2014-01-29 11:25:23 +0900

    Fix bug in translation/gloss deletion of non-existent item

--- api/commenter/class/deleteonerule/matchExec.js
+++ api/commenter/class/deleteonerule/matchExec.js
@@ -16,49 +16,53 @@
         function deleteFromRulesToChoices() {
             var sql = 'DELETE FROM rulesToChoices WHERE ruleID=?';
             sys.db.run(sql,[ruleID],function(err){
-                if (err) {response,err,'**classes/deleteonerule(1)'};
+                if (err) {return oops(response,err,'**classes/deleteonerule(1)')};
                 getRuleTranslationID();
             });
         };
         function getRuleTranslationID() {
             var sql = 'SELECT ruleTranslationID FROM ruleTranslations WHERE ruleID=?';
             sys.db.get(sql,[ruleID],function(err,row){
-                if (err||!row) {response,err,'**classes/deleteonerule(2)'};
-                deleteFromRuleTranslationEdits(row.ruleTranslationID);
+                if (err) {return oops(response,err,'**classes/deleteonerule(2)')};
+                if (row && row.ruleTranslationID) {
+                    deleteFromRuleTranslationEdits(row.ruleTranslationID);
+                } else {
+                    getRuleStringID();
+                }
             });
         };
         function deleteFromRuleTranslationEdits(ruleTranslationID){
             var sql = 'DELETE FROM ruleTranslationEdits WHERE ruleTranslationID=?';
             sys.db.run(sql,[ruleTranslationID],function(err){
-                if (err) {response,err,'**classes/deleteonerule(3)'};
+                if (err) {return oops(response,err,'**classes/deleteonerule(3)')};
                 deleteFromRuleTranslations();
             });
         };
         function deleteFromRuleTranslations(){
             var sql = 'DELETE FROM ruleTranslations WHERE ruleID=?;';
             sys.db.run(sql,[ruleID],function(err){
-                if (err) {response,err,'**classes/deleteonerule(4)'};
+                if (err) {return oops(response,err,'**classes/deleteonerule(4)')};
                 getRuleStringID();
             });
         };
         function getRuleStringID() {
             var sql = 'SELECT ruleStringID FROM rules WHERE ruleID=?;';
             sys.db.get(sql,[ruleID],function(err,row){
-                if (err||!row) {response,err,'**classes/deleteonerule(5)'};
+                if (err||!row) {return oops(response,err,'**classes/deleteonerule(5)')};
                 deleteFromRules(row.ruleStringID);
             });
         };
         function deleteFromRules(ruleStringID) {
             var sql = 'DELETE FROM rules WHERE ruleID=?;';
             sys.db.run(sql,[ruleStringID],function(err){
-                if (err) {response,err,'**classes/deleteonerule(6)'};
+                if (err) {return oops(response,err,'**classes/deleteonerule(6)')};
                 checkForRuleStrings(ruleStringID);
             });
         };
         function checkForRuleStrings(ruleStringID) {
             var sql = 'SELECT COUNT(*) AS count FROM rules WHERE ruleStringID=?;';
             sys.db.get(sql,[ruleStringID],function(err,row){
-                if (err||!row) {response,err,'**classes/deleteonerule(7)'};
+                if (err||!row) {return oops(response,err,'**classes/deleteonerule(7)')};
                 if (row.count == 0) {
                     deleteFromRuleStrings(ruleStringID);
                 } else {
@@ -71,7 +75,7 @@
         function deleteFromRuleStrings(ruleStringID) {
             var sql = 'DELETE FROM ruleStrings WHERE ruleStringID=?;';
             sys.db.run(sql,[ruleStringID],function(err){
-                if (err) {response,err,'**classes/deleteonerule(8)'};
+                if (err) {return oops(response,err,'**classes/deleteonerule(8)')};
                 //console.log("deleting ruleStringID: "+ruleStringID);
                 response.writeHead(200, {'Content-Type': 'applicaton/json'});
                 response.end(JSON.stringify(['success']));
