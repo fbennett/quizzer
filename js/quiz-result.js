1787ac36bf159078794f092fe465d3330cb83b48
Frank Bennett <biercenator@gmail.com>
2014-02-01 13:09:10 +0900

    Tweak student rules view (1)

--- js/quiz-result.js
+++ js/quiz-result.js
@@ -230,11 +230,11 @@ function openRule (node) {
         deleteButton.style.display = 'inline';
     }
     var tr = document.createElement('tr');
-    tr.innerHTML = '<td class="show-box">' + markdown(row.stringOrig) + '</td><td class="edit-box"><textarea>' + row.stringTrans + '</textarea></td>'
+    tr.innerHTML = '<td class="show-box"><div class="show-box-child">' + markdown(row.stringOrig) + '</div></td><td class="edit-box"><textarea>' + row.stringTrans + '</textarea></td>'
     rownode.parentNode.insertBefore(tr,rownode.nextSibling);
     node.setAttribute('onclick','void(0);');
-    setChildHeight(rownode.nextSibling.childNodes[1].childNodes[0]);
-    setChildHeight(rownode.nextSibling.childNodes[0].childNodes[0]);
+    setChildHeight(tr.childNodes[1].childNodes[0]);
+    setChildHeight(tr.childNodes[0].childNodes[0]);
 };
 
 function saveRule (node) {
