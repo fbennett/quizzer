cfe9c548191fc6ddc7b8cb6f95fe871d230ccb54
Frank Bennett <biercenator@gmail.com>
2014-02-01 14:22:45 +0900

    Align details of commenter and student rule views

--- js/quiz-result.js
+++ js/quiz-result.js
@@ -233,8 +233,8 @@ function openRule (node) {
     tr.innerHTML = '<td class="show-box"><div class="show-box-child">' + markdown(row.stringOrig) + '</div></td><td class="edit-box"><textarea>' + row.stringTrans + '</textarea></td>'
     rownode.parentNode.insertBefore(tr,rownode.nextSibling);
     node.setAttribute('onclick','void(0);');
-    setChildHeight(tr.childNodes[1].childNodes[0]);
-    setChildHeight(tr.childNodes[0].childNodes[0]);
+    setChildHeight(rownode.nextSibling.childNodes[1].childNodes[0]);
+    setChildHeight(rownode.nextSibling.childNodes[0].childNodes[0]);
 };
 
 function saveRule (node) {
