88e897a17a5bcfa08b4463e3ff6fc6136f3e9872
Frank Bennett <biercenator@gmail.com>
2014-02-04 17:25:55 +0900

    Use saveonerule logic in student rule edit view as well

--- api/quiz/quiz/saveonerule/matchExec.js
+++ api/quiz/quiz/saveonerule/matchExec.js
@@ -3,9 +3,7 @@
     cogClass.prototype.exec = function (params, request, response) {
         var oops = this.utils.apiError;
         var sys = this.sys;
-        //var classID = params.classid;
-        //var studentID = params.studentid;
-        //var studentKey = params.studentkey;
+        var commenterID = this.sys.validCommenter(params).id;
         var glossText = params.glosstext;
         var ruleText = params.ruletext;
         var ruleID = params.ruleid;
@@ -16,30 +14,104 @@
         if (glossText) {
             glossText = glossText.replace(/^\s+/,'').replace(/\s+$/,'');
         }
-        // XXX This will cause database creep. Should obtain ID first!
-        var sql = 'INSERT OR REPLACE INTO ruleTranslations VALUES (NULL,?,?,?);'
-        sys.db.run(sql,[ruleID,glossText,lang],function(err){
+
+        // * check if there is a rule for this ruleID, and if not, abort [done]
+        // * if the headline text of the rule has changed, update it     [done]
+        // * check if there is a row for this translation                []
+        //   - if no row exists, create it
+        //   - if a row exists, replace it with this text
+        // * refetch rule translations for this language
+        // * return the result
+
+        sys.db.run('BEGIN TRANSACTION',function(err){
             if (err) {return oops(response,err,'**classes/saveonerule(1)')};
-            if (ruleText) {
-                getRuleStringID(ruleID,ruleText);
-            } else {
-                returnText();
-            }
+            checkRule();
         });
-        function getRuleStringID (ruleID,ruleText) {
-            var sql = 'SELECT ruleStringID FROM rules WHERE ruleID=?';
+        
+        function checkRule() {
+            var sql = 'SELECT string FROM rules JOIN ruleStrings USING(ruleStringID) WHERE ruleID=?;';
             sys.db.get(sql,[ruleID],function(err,row){
                 if (err||!row) {return oops(response,err,'**classes/saveonerule(2)')};
-                saveRuleText(row.ruleStringID,ruleText);
+                var oldRuleText = row.string;
+                if (ruleText && ruleText !== oldRuleText) {
+                    checkRuleText();
+                } else {
+                    checkTranslation();
+                }
+            });
+        }
+
+        function checkRuleText() {
+            var sql = 'SELECT ruleStringID FROM ruleStrings WHERE string=?;'
+            sys.db.get(sql,[ruleID],function(err,row){
+                if (err) {return oops(response,err,'**classes/saveonerule(3)')};
+                if (row && row.ruleStringID) {
+                    updateRule(row.ruleStringID);
+                } else {
+                    addRuleString();
+                }
+            });
+        }
+
+        function addRuleString() {
+            var sql = 'INSERT INTO ruleStrings VALUES (NULL,?)';
+            sys.db.run(sql,[ruleText],function(err){
+                if (err) {return oops(response,err,'**classes/saveonerule(4)')};
+                updateRule(this.lastID);
+            });
+        }
+
+        function updateRule(ruleStringID) {
+            var sql = 'UPDATE rules SET ruleStringID=? WHERE ruleID=?;'
+            sys.db.run(sql,[ruleStringID,ruleID],function(err){
+                if (err) {return oops(response,err,'**classes/saveonerule(5)')};
+                checkTranslation();
+            });
+        }
+
+        function checkTranslation() {
+            var sql = 'SELECT ruleTranslationID,string FROM ruleTranslations WHERE ruleID=? AND lang=?;'
+            sys.db.get(sql,[ruleID,lang],function(err,row){
+                if (err) {return oops(response,err,'**classes/saveonerule(6)')};
+                if (row && row.ruleTranslationID) {
+                    if (glossText !== row.string) {
+                        updateTranslation(row.ruleTranslationID);
+                    } else {
+                        endTransaction();
+                    }
+                } else {
+                    if (glossText) {
+                        addTranslation();
+                    } else {
+                        endTransaction();
+                    }
+                }
+            });
+        }
+
+        function addTranslation() {
+            var sql = 'INSERT INTO ruleTranslations VALUES (NULL,?,?,?);'
+            sys.db.run(sql,[ruleID,glossText,lang],function(err){
+                if (err) {return oops(response,err,'**classes/saveonerule(7)')};
+                endTransaction();
             });
         };
-        function saveRuleText (ruleStringID,ruleText) {
-            var sql = 'INSERT OR REPLACE INTO ruleStrings VALUES (?,?);';
-            sys.db.run(sql,[ruleStringID,ruleText],function(err){
-                if (err) {return oops(response,err,'**classes/saveonerule(3)')};
+
+        function updateTranslation(ruleTranslationID) {
+            var sql = 'UPDATE ruleTranslations SET string=? WHERE ruleTranslationID=?;';
+            sys.db.run(sql,[glossText,ruleTranslationID],function(err){
+                if (err) {return oops(response,err,'**classes/saveonerule(8)')};
+                endTransaction();
+            });
+        };
+
+        function endTransaction() {
+            sys.db.run('END TRANSACTION',function(err){
+                if (err) {return oops(response,err,'**classes/saveonerule(9)')};
                 returnText();
             });
         };
+        
         function returnText () {
             var sql = 'SELECT ruleStrings.string AS ruleText,'
                 + 'orig.string AS stringOrig,'
@@ -50,10 +122,7 @@
                 + 'LEFT JOIN (SELECT ruleID,string FROM ruleTranslations WHERE ruleID=? AND lang=?) AS trans USING(ruleID) '
                 + 'WHERE ruleID=?'
             sys.db.get(sql,[ruleID,ruleID,lang,ruleID],function(err,row){
-                if (err) {return oops(response,err,'**classes/saveonerule(2)')};
-                if (!row) {
-                    row = {stringOrig:'',stringTrans:''};
-                }
+                if (err||!row) {return oops(response,err,'**classes/saveonerule(10)')};
                 response.writeHead(200, {'Content-Type': 'application/json'});
                 response.end(JSON.stringify(row));
             });
