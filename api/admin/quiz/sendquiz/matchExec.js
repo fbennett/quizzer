f69e10ee937dbd7d416e240d895b6e8112af0665
Frank Bennett <biercenator@gmail.com>
2014-02-06 07:26:01 +0900

    Refactor quiz mailing code. Distinguish answered and unanswered quizzes in message.

--- api/admin/quiz/sendquiz/matchExec.js
+++ api/admin/quiz/sendquiz/matchExec.js
@@ -13,6 +13,7 @@
         var port = ':' + this.sys.real_port;
         var email_account = this.sys.email_account;
         var studentCount = 0;
+        var mailData = {};
 
         // Hack for reverse proxy support
         var stub = '/';
@@ -20,139 +21,225 @@
             port = '';
             stub = pathName.replace(/(.*\/).*/, '$1/quiz.html');
         } 
-        var front_template = "@@NAME@@,\n\nWe have prepared a fresh quiz to help you check and improve your English writing ability.\n\n"
-            + "Click on the link below to take the quiz:\n\n"
-        var template_link = "    http://" + hostname + port + stub
-            + "?studentid=@@STUDENT_ID@@&studentkey=@@STUDENT_KEY@@"
-            + "&classid=" + classID 
-            + "&quizno=@@QUIZ_NUMBER@@\n\n"
-        var template_middle = "The links to your past quizzes for this class have changed. Here are the new links:\n\n"
-            + "@@LINKS@@"
-        var back = "Sincerely yours,\n"
-            + "The Academic Writing team"
-
+        var templateMail = sys.fs.readFileSync('quiz-message.txt').toString();
+        var templatePastQuizzes = sys.fs.readFileSync('quiz-message-past-quizzes.txt').toString();
+        var templateAnswered = sys.fs.readFileSync('quiz-message-answered.txt').toString();
+        var templateUnanswered = sys.fs.readFileSync('quiz-message-unanswered.txt').toString();
+        var templateLink = '  http://@@HOSTNAME@@@@PORT@@@@STUB@@?studentid=@@STUDENT_ID@@&studentkey=@@STUDENT_KEY@@&classid=@@CLASS_ID@@&quizno=@@QUIZ_NUMBER@@\n';
         // Okay.
-        // Get list of recipients
+        // Get the class name
+        // Get a list of recipient students
         // update recipient keys
-        // 
-        var sql = 'SELECT memberships.studentID,students.name,email,classes.name AS className '
-            + 'FROM memberships '
-            + 'NATURAL JOIN students '
-            + 'JOIN classes USING(classID) '
-            + 'JOIN quizzes USING(classID) '
-            + 'WHERE memberships.classID=? AND quizzes.quizNumber=? '
-            +   'AND ('
-            +     'sent=0 '
-            +     'OR ('
-            +       'sent=1 AND ('
-            +         'last_mail_date IS NULL '
-            +         "OR JULIANDAY('now')-JULIANDAY(last_mail_date)>6"
-            +       ') '
-            +       'AND memberships.studentID NOT IN ('
-            +         'SELECT studentID '
-            +           'FROM quizzes '
-            +           'NATURAL JOIN questions '
-            +           'NATURAL JOIN answers '
-            +           'WHERE quizzes.classID=? '
-            +           'AND quizNumber=? '
-            +           'GROUP BY studentID'
-            +       ')'
-            +     ')'
-            +   ');';
-        // Get students for processing
-        sys.db.all(sql,[classID,quizNumber,classID,quizNumber],function(err,rows){
-            if (err||!rows) {return oops(response,err,'quiz/sendquiz(1)')};
-            var datalst = [];
-            studentCount += rows.length;
-            for (var i=0,ilen=rows.length;i<ilen;i+=1) {
-                var row = rows[i];
-                var studentID = row.studentID;
-                var name = row.name;
-                var email = row.email;
-                var className = row.className;
-                // This is a list of students to receive mail in this class.
-                // Keys need to be updated for all keys of these students, in this class.
-                updateStudentKey(classID,studentID,email,name,className);
-            }
-        });
+        // For each student, get a list of quizzes completed
+        // For each student, get a list of quizzes not yet completed
+        // Fire off the mailing
+        beginTransaction();
 
-        function updateStudentKey (classID,studentID,email,name,className) {
+        function beginTransaction(){
+            sys.db.run('BEGIN TRANSACTION',function(err){
+                if (err){return oops(response,err,'quiz/sendquiz(1)')}
+                getClassName();
+            });
+        };
+
+        function getClassName(){
+            var sql = 'SELECT name FROM classes WHERE classID=?;';
+            sys.db.get(sql,[classID],function(err,row){
+                if (err||!row){return oops(response,err,'quiz/sendquiz(2)')}
+                mailData.className = row.name;
+                getStudents();
+            });
+        };
+
+        function getStudents(){
+            mailData.students = [];
+            var sql = 'SELECT memberships.studentID,students.name,email,classes.name AS className '
+                + 'FROM memberships '
+                + 'JOIN students USING(studentID) '
+                + 'JOIN classes ON classes.classID=memberships.classID '
+                + 'JOIN quizzes ON quizzes.classID=memberships.classID '
+                + 'WHERE memberships.classID=? AND quizzes.quizNumber=? '
+                +   'AND ('
+                +     'sent=0 '
+                +     'OR ('
+                +       'sent=1 AND ('
+                +         'last_mail_date IS NULL '
+                +         "OR JULIANDAY('now')-JULIANDAY(last_mail_date)>6"
+                +       ') '
+                +       'AND memberships.studentID NOT IN ('
+                +         'SELECT studentID '
+                +           'FROM quizzes '
+                +           'NATURAL JOIN questions '
+                +           'NATURAL JOIN answers '
+                +           'WHERE quizzes.classID=? '
+                +           'AND quizNumber=? '
+                +           'GROUP BY studentID'
+                +       ')'
+                +     ')'
+                +   ');';
+            sys.db.all(sql,[classID,quizNumber,classID,quizNumber],function(err,rows){
+                if (err||!rows){return oops(response,err,'quiz/sendquiz(3)')}
+                for (var i=0,ilen=rows.length;i<ilen;i+=1) {
+                    var row = rows[i];
+                    mailData.students.push(row);
+                }
+                updateStudentKeys(0,rows.length);
+            });
+        };
+        
+        function updateStudentKeys (pos,limit) {
+            var student = mailData.students[pos];
+            var studentID = student.studentID;
             var studentKey = sys.getRandomKey(8,36);
             sys.db.run('UPDATE memberships SET studentKey=? WHERE classID=? AND studentID=?',[studentKey,classID,studentID],function(err){
-                if (err) {return oops(response,err,'quiz/sendquiz(2)')};
+                if (err) {return oops(response,err,'quiz/sendquiz(4)')};
                 // Good, so this does that. Flag as sent, and send the mail message.
                 sys.membershipKeys[classID][studentID] = studentKey;
-                sendMail(quizNumber,studentID,studentKey,name,email,className);
-            });
-        };
-
-        function sendMail (quizNumber,studentID,studentKey,name,email,className) {
-            var link = template_link
-                .replace(/@@STUDENT_ID@@/g,studentID)
-                .replace(/@@STUDENT_KEY@@/g,studentKey)
-                .replace(/@@QUIZ_NUMBER@@/g,quizNumber);
-            var front = front_template.replace(/@@NAME@@/,name)
-            var pastLinks = 0;
-            var sql = 'SELECT quizNumber '
-                + 'FROM quizzes '
-                + 'NATURAL JOIN questions '
-                + 'WHERE classID=? AND NOT quizNumber=? AND sent=1 GROUP BY quizNumber'
-            sys.db.all(sql,[classID,quizNumber],function(err,rows){
-                if (err) {return oops(response,err,'quiz/sendquiz(3)')};
-                var middle = '';
-                var msg;
-                if (!rows || !rows.length) {
-                    // plain message
-                    msg = front + link + back;
+                mailData.students[pos].studentKey = studentKey;
+                pos += 1;
+                if (pos < limit) {
+                    updateStudentKey(pos,limit);
                 } else {
-                    // add note of past quiz links
-                    var links = '';
-                    var otherQuizNumber;
-                    for (var i=0,ilen=rows.length;i<ilen;i+=1) {
-                        var row = rows[i];
-                        otherQuizNumber = row.quizNumber;
-                        links += template_link
-                            .replace(/@@STUDENT_ID@@/g,studentID)
-                            .replace(/@@STUDENT_KEY@@/g,studentKey)
-                            .replace(/@@QUIZ_NUMBER@@/g,otherQuizNumber);
-                    }
-                    var middle = template_middle
-                        .replace(/@@LINKS@@/g,links);
-                    msg = front + link + middle + back;
+                    getQuizLinks(0,limit);
                 }
-                //console.log(msg);
-                sys.mailer.send({
-                    text:    msg, 
-                    from:    "Instructor <" + email_account + ">", 
-                    to:      email,
-                     subject: className + ': Quiz ' + quizNumber
-                }, function(err, message) { console.log(err || message); });
-
-                refreshDateStamp(classID,studentID);
-            });
-        }
-
-        function refreshDateStamp(classID,studentID) {
-            sys.db.run('UPDATE memberships SET last_mail_date=DATE("now") WHERE classID=? AND studentID=?',[classID,studentID],function(err){
-                if (err) {return oops(response,err,'quiz/sendquiz(4)')};
-                studentCount += -1;
-                if (!studentCount) {
-                    updateSentFlag(classID,quizNumber);
-                }
-            });
-        }
-
-        function updateSentFlag (classID,quizNumber) {
-            sys.db.run('UPDATE quizzes SET sent=1 WHERE classID=? AND quizNumber=?',[classID,quizNumber],function(err){
-                if (err) {return oops(response,err,'quiz/sendquiz(5)')};
-                //console.log("Updated SENT flag");
             });
         };
 
+        function getQuizLinks(pos,limit) {
+            // Get quizzes that have been answered by a given student
+            // and quizzes that have not been.
+            var sql = 'SELECT quizNumber,'
+                + 'CASE WHEN COUNT(answers.answerID)>0 THEN 1 ELSE 0 END AS answered '
+                + 'FROM students '
+                + 'JOIN quizzes '
+                + 'JOIN questions USING(quizID) '
+                + 'LEFT JOIN answers ON answers.studentID=students.studentID AND answers.questionID=questions.questionID '
+                + 'WHERE classID=? AND students.studentID=? AND NOT quizNumber=? AND sent=1 '
+                + 'GROUP BY quizzes.quizID;'
+            var studentID = mailData.students[pos].studentID;
+            var studentID = mailData.students[pos].studentID;
+            mailData.students[pos].answered = [];
+            mailData.students[pos].unanswered = [];
+            sys.db.all(sql,[classID,studentID,quizNumber],function(err,rows){
+                if (err||!rows) {return oops(response,err,'quiz/sendquiz(5)')};
+                for (var i=0,ilen=rows.length;i<ilen;i+=1) {
+                    var row = rows[i];
+                    if (row.answered == 1) {
+                        mailData.students[pos].answered.push(row.quizNumber);
+                    } else {
+                        mailData.students[pos].unanswered.push(row.quizNumber);
+                    }
+                }
+                pos += 1;
+                if (pos < limit) {
+                    getQuizLinks(pos,limit);
+                } else {
+                    response.writeHead(500, {'Content-Type': 'application/json'});
+                    response.end(JSON.stringify(['success']));
+                    sendMail(0,limit);
+                }
+            });
+        };
 
-        //
-        response.writeHead(500, {'Content-Type': 'text/plain'});
-        response.end("Testing ...");
+        function sendMail (pos,limit) {
+            var student = mailData.students[pos];
+            var mailText = templateMail
+                .replace(/@@NAME@@/,student.name)
+                .replace(/@@STUDENT_ID@@/g,student.studentID)
+                .replace(/@@STUDENT_KEY@@/g,student.studentKey)
+                .replace(/@@QUIZ_NUMBER@@/g,quizNumber)
+                .replace(/@@HOSTNAME@@/g,hostname)
+                .replace(/@@PORT@@/g,port)
+                .replace(/@@STUB@@/g,stub)
+                .replace(/@@CLASS_ID@@/g,classID)
+            if (student.answered.length || student.unanswered.length) {
+                mailText = mailText.replace(/@@PAST_QUIZZES@@/g,templatePastQuizzes);
+                var unansweredQuizText = '';
+                if (student.unanswered.length) {
+                    var unansweredQuizLinks = '';
+                    for (var i=0,ilen=student.unanswered.length;i<ilen;i+=1) {
+                        var oldQuizNumber = student.unanswered[i];
+                        var link = templateLink
+                            .replace(/@@NAME@@/,student.name)
+                            .replace(/@@STUDENT_ID@@/g,student.studentID)
+                            .replace(/@@STUDENT_KEY@@/g,student.studentKey)
+                            .replace(/@@QUIZ_NUMBER@@/g,oldQuizNumber)
+                            .replace(/@@HOSTNAME@@/g,hostname)
+                            .replace(/@@PORT@@/g,port)
+                            .replace(/@@STUB@@/g,stub)
+                            .replace(/@@CLASS_ID@@/g,classID)
+                        unansweredQuizLinks += link;
+                    }
+                    unansweredQuizText = templateUnanswered.replace(/@@UNANSWERED_QUIZZES@@/,unansweredQuizLinks);
+                }
+                mailText = mailText.replace(/@@UNANSWERED@@/,unansweredQuizText);
+
+                var answeredQuizText = '';
+                if (student.answered.length) {
+                    var answeredQuizLinks = '';
+                    for (var i=0,ilen=student.answered.length;i<ilen;i+=1) {
+                        var oldQuizNumber = student.answered[i];
+                        var link = templateLink
+                            .replace(/@@NAME@@/,student.name)
+                            .replace(/@@STUDENT_ID@@/g,student.studentID)
+                            .replace(/@@STUDENT_KEY@@/g,student.studentKey)
+                            .replace(/@@QUIZ_NUMBER@@/g,oldQuizNumber)
+                            .replace(/@@HOSTNAME@@/g,hostname)
+                            .replace(/@@PORT@@/g,port)
+                            .replace(/@@STUB@@/g,stub)
+                            .replace(/@@CLASS_ID@@/g,classID)
+                        answeredQuizLinks += link;
+                    }
+                    answeredQuizText = templateAnswered.replace(/@@ANSWERED_QUIZZES@@/,answeredQuizLinks);
+                }
+                mailText = mailText.replace(/@@ANSWERED@@/,answeredQuizText);
+
+            } else {
+                mailText = mailText.replace(/@@PAST_QUIZZES@@/g,'');
+            }
+            sys.mailer.send({
+                text:    mailText, 
+                from:    "Instructor <" + email_account + ">", 
+                to:      student.email,
+                subject: mailData.className + ': Quiz ' + quizNumber
+            }, function(err, message) {
+                if (err) {console.log(err)};
+                pos += 1;
+                if (pos < limit) {
+                    sendMail(pos,limit);
+                } else {
+                    refreshDateStamps(0,limit);
+                }
+            });
+        };
+
+        function refreshDateStamps (pos,limit) {
+            var studentID = mailData.students[pos].studentID;
+            sys.db.run('UPDATE memberships SET last_mail_date=DATE("now") WHERE classID=? AND studentID=?',[classID,studentID],function(err){
+                if (err) {return oops(response,err,'quiz/sendquiz(6)')};
+                pos += 1;
+                if (pos < limit) {
+                    refreshDateStamps(pos,limit);
+                } else {
+                    updateSentFlag();
+                }
+            });
+        }
+
+        function updateSentFlag () {
+            sys.db.run('UPDATE quizzes SET sent=1 WHERE classID=? AND quizNumber=?',[classID,quizNumber],function(err){
+                if (err) {return oops(response,err,'quiz/sendquiz(7)')};
+                //console.log("Updated SENT flag");
+                endTransaction();
+            });
+        };
+
+        function endTransaction () {
+            sys.db.run('END TRANSACTION',function(err){
+                if (err) {return oops(response,err,'quiz/sendquiz(8)')};
+                console.log("Sent quiz mail");
+            });
+        };
     };
     exports.cogClass = cogClass;
 })();
