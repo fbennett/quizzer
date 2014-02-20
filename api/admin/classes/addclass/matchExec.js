c1ac883c33a24bd709f1e17bcaa16fb0871ad5b5
Frank Bennett <biercenator@gmail.com>
2014-02-20 19:56:27 +0900

    Update memory object when adding a class

--- api/admin/classes/addclass/matchExec.js
+++ api/admin/classes/addclass/matchExec.js
@@ -2,21 +2,22 @@
     var cogClass = function () {};
     cogClass.prototype.exec = function (params, request, response) {
         var oops = this.utils.apiError;
+        var sys = this.sys;
         if (params.classid) {
-            var db = this.sys.db;
-            db.run('INSERT OR REPLACE INTO classes VALUES (?,?,?)',[params.classid,params.groupid,params.name],function(err){
+            sys.db.run('INSERT OR REPLACE INTO classes VALUES (?,?,?)',[params.classid,params.groupid,params.name],function(err){
                 if (err) {return oops(response,err,'classes/addclass(1)')};
                 sendClasses();
             })
         } else {
             var db = this.sys.db;
-            db.run('INSERT INTO classes VALUES (NULL,?,?)',[params.groupid,params.name],function(err){
+            sys.db.run('INSERT INTO classes VALUES (NULL,?,?)',[params.groupid,params.name],function(err){
                 if (err) {return oops(response,err,'classes/addclass(2)')};
+                sys.membershipKeys[this.lastID] = {};
                 sendClasses();
             });
         }
         function sendClasses () {
-            db.all('SELECT classID,classes.name,ruleGroupID,ruleGroups.name AS ruleGroupName FROM classes JOIN ruleGroups USING(ruleGroupID) ORDER BY classes.name;',function(err,rows){
+            sys.db.all('SELECT classID,classes.name,ruleGroupID,ruleGroups.name AS ruleGroupName FROM classes JOIN ruleGroups USING(ruleGroupID) ORDER BY classes.name;',function(err,rows){
                 if (err||!rows) {return oops(response,err,'classes/addclass(3)')};
                 response.writeHead(200, {'Content-Type': 'application/json'});
                 response.end(JSON.stringify(rows));
