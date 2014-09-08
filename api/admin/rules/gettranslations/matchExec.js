16904b82934e21e850088d145975725e4076787e
Frank Bennett <biercenator@gmail.com>
2014-09-08 10:36:51 +0900

    Implement header styling in RTF exports of rules

--- api/admin/rules/gettranslations/matchExec.js
+++ api/admin/rules/gettranslations/matchExec.js
@@ -55,27 +55,38 @@
                 obj.push(misc);
             }
             // Good. So now we have the content, in sections, in Markdown, ready for catenating.
-            var txt = '## Style and Grammar Notes for ' + langName + '\n\n';
+            var txt = '# Style and Grammar Notes for ' + langName + '\n\n';
             for (var i=0,ilen=obj.length;i<ilen;i++) {
                 var section = obj[i];
-                txt += '### ' + section.category + '\n\n';
+                txt += '## ' + section.category + '\n\n';
                 for (var j=0,jlen=section.entries.length;j<jlen;j++) {
                     var entry = section.entries[j];
-                    txt += '#### ' + entry.heading + '\n\n';
+                    txt += '### ' + entry.heading + '\n\n';
                     txt += entry.text + '\n\n';
                 }
             }
             // Convert to HTML
             var html = sys.markdown(txt,true,true);
             // Some RTF formatting templates
-            var rtfHeader = "{\\rtf1\\ansi\n";
+            var rtfHeader = "{\\rtf1\\ansi\\deff0{\\fonttbl{\\f0 \\fswiss Helvetica;}{\\f1 \\fmodern Courier;}}\n"
+                + "{\\colortbl;\\red0\\green0\\blue0;\\red255\\green0\\blue0;\\red100\\green160\\blue100;\\red0\\green0\\blue255;}\n"
+                + "{\\stylesheet "
+                +   "{\\s0 \\ql \\f0 \\fs24 \\sa180 \\li0 \\fi0;}"
+                +   "{\\s1 \\sbasedon0 \\b\\fs36 \\sa180 \\outlinelevel1 Heading1;}"
+                +   "{\\s2 \\sbasedon0 \\b\\fs32 \\sa180 \\outlinelevel2 Heading2;}"
+                +   "{\\s3 \\sbasedon0 \\b\\fs28 \\sa180 \\outlinelevel3 Heading3;}"
+                +   "{\\s4 \\sbasedon0 \\b\\fs24 \\sa180 \\outlinelevel4 Heading4;}"
+                +   "{\\s5 \\sbasedon0 \\b\\fs20 \\sa180 \\outlinelevel5 Heading5;}"
+                +   "{\\s10 \\sbasedon0 \\f1 \\fs24 \\sa180 Code;}"
+                + "}\n"
+                + "\\widowctrl\\hyphauto\n";
             var rtfColorTable = "{\\colortbl;\\red0\\green0\\blue0;\\red255\\green0\\blue0;\\red100\\green160\\blue100;\\red0\\green0\\blue255;}";
             var rtfBoxedIncorrect = "\\li720\\ri720\\box\\brdrdot\\brdrw10\\brsp20\\brdrcf2";
             var rtfBoxedIncorrectList = "\\li1080\\ri720\\box\\brdrdot\\brdrw10\\brsp20\\brdrcf2";
             var rtfBoxedCorrect = "\\li720\\ri720\\box\\brdrdb\\brdrw10\\brsp20\\brdrcf3";
             var rtfBoxedCorrectList = "\\li1080\\ri720\\box\\brdrdb\\brdrw10\\brsp20\\brdrcf3";
             var rtfBoxedRule = "\\li720\\ri720\\box\\brdrs\\brdrw10\\brsp20\\brdrcf1";
-            var rtfTrailer = "\\par}"
+            var rtfTrailer = "}"
 
             // And now convert it to RTF
             sys.pandoc.convert('html',html,['rtf'],function(result, err){
@@ -90,6 +101,10 @@
                 rtf = rtf.replace(/\\li2160/g,rtfBoxedRule);
                 rtf = rtf.replace(/\\li1080/g,rtfBoxedIncorrectList);
                 rtf = rtf.replace(/\\li1800/g,rtfBoxedCorrectList);
+                rtf = rtf.replace(/\\ql \\f0 \\sa180 \\li0 \\fi0 \\b \\fs32/g,"\\s2");
+                rtf = rtf.replace(/\\ql \\f0 \\sa180 \\li0 \\fi0 \\b \\fs28/g,"\\s3");
+                rtf = rtf.replace(/\\ql \\f0 \\sa180 \\li0 \\fi0 \\b \\fs24/g,"\\s4");
+                rtf = rtf.replace(/\\ql \\f0 \\sa180 \\li0 \\fi0 \\b \\fs20/g,"\\s5");
                 // Header and footer
                 rtf = rtfHeader + rtfColorTable + rtf + rtfTrailer;
                 // And ship it for downloading
